c73d0571d9c2be8b02e52943611899ddfcb99a62
Ben Hall <benjamhall@gmail.com>
2021-05-02 15:43:18 -0500

    edit #5

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -54,5 +54,6 @@ console.log('updated shoe count', me.shoeCount);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
 me.favoriteColor++;
 console.log('updated', me);
